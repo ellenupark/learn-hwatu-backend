de5f612d4a502986da8acd45c9642f5b41c28f10
ellenupark <ellenupark@gmail.com>
2020-12-19 01:48:13 -0500

    Added history and root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :cards
   resources :players
   resources :games
+
+  get 'games/history', to: 'games#history'
+
+  root to: "cards#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
