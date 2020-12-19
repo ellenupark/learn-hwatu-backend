18f91384b34edaf4fcc955f1dfb36f47c7a3d1ae
ellenupark <ellenupark@gmail.com>
2020-12-18 20:58:04 -0500

    Created game and player models

---
+++ db/migrate/20201219015225_create_games.rb
@@ -0,0 +1,10 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.string :name
+      t.integer :points
+
+      t.timestamps
+    end
+  end
+end
