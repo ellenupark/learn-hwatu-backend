80c5d6e7c298a0089a91a5b2d25d7d1bb2760e62
ellenupark <ellenupark@gmail.com>
2020-12-19 01:46:52 -0500

    Added validations and scope method for top ten games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
+    validates :name, presence: true
+    validates :name, length: { maximum: 30 }
     has_many :players, dependent: :destroy
+
+    scope :retrieve_top_ten_games, -> { order(points: :desc).limit(10) }
 end
