9e4b6499d66a464dcd96af4acec7946ee2baf2f9
ellenupark <ellenupark@gmail.com>
2020-12-18 21:37:23 -0500

    Added game controller actions

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,37 @@
 class GamesController < ApplicationController
+    def index
+        games = Game.all
+        render json: GameSerializer.new(games)
+    end
+
+    # def history
+    #     games = Game.retrieve_top_ten_games
+    #     render json: GameSerializer.new(games)
+    # end
+
+    # def create
+    #     game = Game.new(game_params)
+
+    #     if game.save
+    #         render json: GameSerializer.new(game)
+    #     else
+    #         render json: {errors: game.errors.full_messages.to_sentence}
+    #     end
+    # end
+
+    # def update
+    #     game = Game.find_by_id(params[:id])
+    #     if game.update(game_params);
+    #         render json: GameSerializer.new(game)
+    #     else 
+    #         render json: {errors: game.errors.full_messages.to_sentence}
+    #     end
+    # end
+
+
+    private
+
+    def game_params
+        params.require(:game).permit(:name, :points)
+    end
 end
