b6c17cc376c85ad9f7a55c8dc18256d69374626f
ellenupark <ellenupark@gmail.com>
2020-12-28 18:25:26 -0500

    Updated README

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   get 'games/history', to: 'games#history'
 
   root to: "cards#index"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
